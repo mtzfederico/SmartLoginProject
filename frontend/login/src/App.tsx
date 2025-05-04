678ae6df62d3c00f7b82ffbf1f602a85b48dcab0
Tatiana Fernandez <tferna03@nyit.edu>
2025-05-04 14:55:21 -0400

    added navbar (back button)

--- frontend/login/src/App.tsx
+++ frontend/login/src/App.tsx
@@ -1,26 +1,32 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Page from './page.tsx'
-import SwipePage from './swipePageDir/swipePage.tsx'
-import SelectionPage from './selectionPageDir/selectionPage.tsx'
-import CoursePage from './courseSelectionPageDir/courseSelectionPage.tsx'
-import CourseHome from './courseSelectionPageDir/courseHome.tsx'
-import CourseRecords from './courseSelectionPageDir/courseRecords.tsx'
-import CourseRedirect from './courseSelectionPageDir/courseRedirect.tsx'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Page from './page.tsx';
+import SwipePage from './swipePageDir/swipePage.tsx';
+import SelectionPage from './selectionPageDir/selectionPage.tsx';
+import CoursePage from './courseSelectionPageDir/courseSelectionPage.tsx';
+import CourseHome from './courseSelectionPageDir/courseHome.tsx';
+import CourseRecords from './courseSelectionPageDir/courseRecords.tsx';
+import CourseRedirect from './courseSelectionPageDir/courseRedirect.tsx';
+import Layout from './Layout.tsx'; 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                {/* Routes without navbar */}
                 <Route path="/" element={<Page />} />
+
+                {/* Routes WITH navbar */}
+                <Route element={<Layout />}>
                 <Route path="/swipe" element={<SwipePage />} />
                 <Route path="/selection" element={<SelectionPage />} />
+                <Route path="/redirect" element={<CourseRedirect />} />
                 <Route path="/course-selection" element={<CoursePage />} />
                 <Route path="/course-home" element={<CourseHome />} />
                 <Route path="/course-records" element={<CourseRecords />} />
-                <Route path="/redirect" element={<CourseRedirect />} />
+                </Route>
             </Routes>
         </BrowserRouter>
-    )
+    );
 }
 
-export default App
+export default App;
+
